d7416096e43dbe452aa4a56e87ca71c2b6a96c1e
mark jad <mark.jad@live.com>
2017-12-21 12:11:23 +0200

    Add specs

--- spec/farsi_normalizer_spec.rb
+++ spec/farsi_normalizer_spec.rb
@@ -5,13 +5,24 @@ describe FarsiNormalizer do
     expect(FarsiNormalizer::VERSION).not_to be nil
   end
 
-  it "normalize letters from beginning of word" do
+  it "normalize arabic letters to farsi" do
     data = {
-      "کتلت" => "كتلت"
+      "كتلت" => "کتلت",
+      "بسکويت" => "بسکویت",
+      "بسکوىت" => "بسکویت",
+      "كىساديا" => "کیسادیا"
     }
 
     data.each do |before, after|
       expect(FarsiNormalizer.normalize(before)).to eq(after)
     end
+  end
+
+  it "has an only character option" do
+    expect(FarsiNormalizer.normalize("بسكويت", only: ["ك"])).to eq("بسکويت")
+  end
+
+  it "has an expect character option" do
+    expect(FarsiNormalizer.normalize("بسكويت", except: ["ك"])).to eq("بسكویت")
   end
 end
